64ad160bebcaf44d1f72d57e747384963d912832
JeffJobs <jeff.jobs.ti@outlook.com>
2024-08-05 23:03:09 -0300

    Desenvolvendo To-Do List App

---
+++ dia-2/to-do-list/js/script.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("Você deve escrever alguma coisa!");
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
